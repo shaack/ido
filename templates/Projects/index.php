ce7760b6048502392b2aed4bc765cd71d1e07033
shaack <shaack@gmail.com>
2023-12-20 17:15:00 +0100

    added start und end to projects list

--- templates/Projects/index.php
+++ templates/Projects/index.php
@@ -13,9 +13,11 @@
                     <th><?= $this->Paginator->sort('name') ?></th>
                     <th><?= $this->Paginator->sort('customer_id') ?></th>
                     <th><?= $this->Paginator->sort('invoice_number', 'Inv. #') ?></th>
-                    <th><?= $this->Paginator->sort('invoice_date', 'Inv. date') ?></th>
                     <th><?= $this->Paginator->sort('project_status_id', 'Status') ?></th>
                     <th class="text-end"><?= $this->Paginator->sort('costs') ?></th>
+                    <th><?= $this->Paginator->sort('start', 'Start') ?></th>
+                    <th><?= $this->Paginator->sort('end', 'End') ?></th>
+                    <th><?= $this->Paginator->sort('invoice_date', 'Inv. date') ?></th>
                     <th class="actions"><?= __('Actions') ?></th>
                 </tr>
             </thead>
@@ -26,11 +28,13 @@
                     <?php $customer = $project->customer ?>
                     <td><?= $this->Html->link($customer->shortcut, ['controller' => 'Customers', 'action' => 'view', $customer->id], ['style' => 'color: ' . $customer->color]) ?></td>
                     <td><?= h($project->invoice_number) ?></td>
-                    <td><?= h($project->invoice_date) ?></td>
                     <td class="project-status-<?= $project->project_status->id ?>">
                         <?= $this->Html->link($project->project_status->name, ['controller' => 'ProjectStatuses', 'action' => 'view', $project->project_status->id]) ?>
                     </td>
                     <td class="text-end"><?= $project->costs() > 0 ? $this->Number->currency($project->costs()) : "" ?></td>
+                    <td><?= $project->start ?></td>
+                    <td><?= $project->end ?></td>
+                    <td><?= h($project->invoice_date) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $project->id]) ?>
                     </td>
