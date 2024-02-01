822b69ec45207982dc4aa4996849b293b819869a
shaack <shaack@gmail.com>
2024-02-01 14:47:01 +0100

    time tracking export optimized

--- scripts/reports/time_trackings.sql
+++ scripts/reports/time_trackings.sql
@@ -1,8 +1,8 @@
-SELECT tt.created as Start, tt.duration as 'Dauer (h)', ts.name as Task, sv.name as Leistung, pr.name as Project
+SELECT tt.created as Start, tt.duration as 'Dauer (h)', sv.name as Leistung, ts.name as Task, pr.name as Project
 FROM time_trackings tt
          LEFT JOIN tasks ts ON ts.id = tt.task_id
          LEFT JOIN services sv ON ts.service_id = sv.id
         LEFT JOIN projects pr ON sv.project_id = pr.id
-WHERE pr.id = 374 ORDER by start
+WHERE pr.id = 379 AND tt.duration > 0 ORDER by start
 
 # Dateiname "FUZ-Zeiterfassung 2023-12.xlsx"
