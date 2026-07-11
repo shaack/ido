9558ab439d29351396326bb40d2118ea1a5c0ff9
shaack <shaack@gmail.com>
2026-07-11 22:24:04 +0200

    Aktionen der TimeTracking-Bearbeitung auf das Standard-Spacing
    Ich hatte die Seite mit side-nav und side-nav-item gebaut. Für diese
    beiden Klassen gibt es im Projekt aber überhaupt keine CSS-Regel, weder
    im kompilierten Stylesheet noch in den SCSS-Quellen. Es sind Reste aus
    dem CakePHP-Skeleton, dessen Stylesheet gegen Bootstrap getauscht wurde.
    Die Links klebten deshalb aneinander.
    
    Das Standard-Spacing kommt aus der einzigen echten Regel des Projekts:
    
        .actions { margin-bottom: 2rem }
        .actions a { display: inline; margin-right: 2rem; text-decoration: none }
    
    Die Seite benutzt jetzt dasselbe Muster wie Tasks/edit.php und
    Services/edit.php: eine .actions-Leiste oben, Delete rechts abgesetzt.

--- web/templates/TimeTrackings/edit.php
+++ web/templates/TimeTrackings/edit.php
@@ -6,32 +6,31 @@
  */
 $this->assign('title', __('Edit Time Tracking'));
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Track'), ['action' => 'track', $timeTracking->id], ['class' => 'side-nav-item']) ?>
+<div class="actions">
+    <div class="row">
+        <div class="col">
+            <?= $this->Html->link(__('Track'), ['action' => 'track', $timeTracking->id]) ?>
+            <?= $this->Html->link(__('List Time Trackings'), ['action' => 'index']) ?>
+        </div>
+        <div class="col-auto">
             <?= $this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $timeTracking->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $timeTracking->id), 'class' => 'side-nav-item text-danger']
+                ['confirm' => __('Are you sure you want to delete # {0}?', $timeTracking->id), 'class' => 'text-danger']
             ) ?>
-            <?= $this->Html->link(__('List Time Trackings'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="timeTrackings form content">
-            <?= $this->Form->create($timeTracking) ?>
-            <fieldset>
-                <legend><?= __('Edit Time Tracking') ?></legend>
-                <?php
-                    echo $this->Form->control('task_id', ['options' => $tasks]);
-                    echo $this->Form->control('created', ['type' => 'datetime-local']);
-                    echo $this->Form->control('duration', ['step' => 'any', 'label' => __('Duration (hours)')]);
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
         </div>
     </div>
+</div>
+<div class="timeTrackings form content">
+    <?= $this->Form->create($timeTracking) ?>
+    <fieldset>
+        <legend><?= __('Edit Time Tracking') ?></legend>
+        <?php
+            echo $this->Form->control('task_id', ['options' => $tasks]);
+            echo $this->Form->control('created', ['type' => 'datetime-local']);
+            echo $this->Form->control('duration', ['step' => 'any', 'label' => __('Duration (hours)')]);
+        ?>
+    </fieldset>
+    <?= $this->Form->button(__('Submit')) ?>
+    <?= $this->Form->end() ?>
 </div>
